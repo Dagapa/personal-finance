5d307808989dd20c93cc0323b515ed99fbc4090d
David Esteban Gallego <dgallego@grupoasd.com>
2025-01-21 19:38:53 -0500

    feat: add header wiframe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,36 +1,32 @@
 'use client';
 
 import styles from "./page.module.css";
-import { baseUrl } from "../config/paths";
-import React, { useEffect, useState } from "react";
+import { Header } from "../src/shared/components/header";
 
 export default function Home() {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch(`${baseUrl}/users`);
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const response = await fetch(`${baseUrl}/users`);
+  //       const data = await response.json();
+  //       setUsers(data);
+  //     } catch (error) {
+  //       console.error("Error fetching users:", error);
+  //     }
+  //   };
 
-    fetchUsers();
-  }, []);
+  //   fetchUsers();
+  // }, []);
 
-  console.log(users);
+  // console.log(users);
 
   return (
-    <div className={styles.page}>
-      {users.map((user: any) => (
-        <div key={user.id}>
-          <p>name: {user.name}</p>
-          <p>email: {user.email}</p>
-          </div>
-      ))}
-    </div>
+    <main className={styles.page}>
+      <Header />
+      <h1>Welcome to Personal Finance</h1>
+
+    </main>
   );
 }
