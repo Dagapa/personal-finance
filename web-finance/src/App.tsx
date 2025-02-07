d5646c9d18f014c480d1816e01fe6143a539c97c
David Esteban Gallego <dgallego@grupoasd.com>
2025-02-06 20:02:41 -0500

    feat: integrate Dashboard component into App and adjust styles

--- web-finance/src/App.tsx
+++ web-finance/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Transaction } from "./components/transaction/transaction";
+import { Dashboard } from "./components/dashboard/dashboard/dashboard";
 
 function App() {
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
@@ -9,7 +10,10 @@ function App() {
   };
 
   return (
-    <main className="bg-slate-800 text-white min-w-screen min-h-screen relative flex flex-col items-center justify-center">
+    <main className="bg-slate-800 text-white min-w-screen min-h-screen relative flex flex-col items-center justify-center p-15">
+
+      <Dashboard />
+      
       <div className={`fixed bg-black bg-opacity-10 flex items-center justify-center transition-opacity duration-300 ${isOpenModal ? "opacity-100 visible" : "opacity-0 invisible"}`}>
         <div className={`bg-white text-black p-6 rounded-lg shadow-lg transition-transform duration-300 ${isOpenModal ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"}`}>
           <button
@@ -24,7 +28,7 @@ function App() {
 
       {!isOpenModal && (
         <button
-          className="px-3 py-2 bg-blue-600 text-white rounded-md shadow-md transition-transform duration-300 hover:scale-105 cursor-pointer"
+          className="absolute right-15 bottom-15 px-3 py-2 bg-blue-600 text-white rounded-4xl shadow-md transition-transform duration-300 hover:scale-105 cursor-pointer"
           onClick={handleOpenModal}
         >
           Agregar transacción
