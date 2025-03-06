ee82c6fbe626d8d931e31e23cb92bd25af443873
David Esteban Gallego <dgallego@grupoasd.com>
2025-03-05 20:29:45 -0500

    feat: refactor dashboard component to use CSS modules and improve styling

--- web-finance/src/components/dashboard/dashboard/dashboard.tsx
+++ web-finance/src/components/dashboard/dashboard/dashboard.tsx
@@ -1,31 +1,31 @@
-import styles from './styles.module.scss';
+import styles from './styles.module.css';
 
 export const Dashboard = () => {
-	return (
-		<section className="grid grid-cols-2 grid-rows-2 place-items-center gap-4 p-4 w-full h-full">
-			<div className="flex flex-col items-center justify-center col-span-2 shadow-md shadow-gray-900 rounded-lg p-4 gap-1.5 w-full h-full">
-				<h2>Balance</h2>
-			</div>
-			<div className="flex items-center justify-center shadow-md shadow-gray-900 rounded-lg p-4 w-full h-full">
-				<h2>Resumen</h2>
-			</div>
-			<div className="flex flex-col items-center justify-center shadow-md shadow-gray-900 rounded-lg p-4 gap-5 w-full h-full">
-				<h2 className="text-xl">Transacciones de hoy</h2>
-				<ul className="flex flex-col gap-4 w-full">
-					<li className="flex items-center center gap-1.5 text-sm text-zinc-300">
-						<span>$ -3000</span>
-						<p>House reparations</p>
-					</li>
-					<li className="flex items-center center gap-2 text-sm text-zinc-300">
-						<span>$ -1000</span>
-						<p>Travel to the Europe</p>
-					</li>
-					<li className="flex items-center center gap-2 text-sm text-zinc-300">
-						<span>$ -20</span>
-						<p>Buy cookies</p>
-					</li>
-				</ul>
-			</div>
-		</section>
-	);
+  return (
+    <section className={styles.dashboard}>
+      <div className={styles.head_boxes}>
+        <h2>Balance</h2>
+      </div>
+      <div className={styles.head_boxes}>
+        <h2>Resumen</h2>
+      </div>
+      <div className={styles.summary}>
+        <h2 className="text-xl">Transacciones de hoy</h2>
+        <ul className="flex flex-col gap-4 w-full">
+          <li className="flex items-center center gap-1.5 text-sm text-zinc-300">
+            <span>$ -3000</span>
+            <p>House reparations</p>
+          </li>
+          <li className="flex items-center center gap-2 text-sm text-zinc-300">
+            <span>$ -1000</span>
+            <p>Travel to the Europe</p>
+          </li>
+          <li className="flex items-center center gap-2 text-sm text-zinc-300">
+            <span>$ -20</span>
+            <p>Buy cookies</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
