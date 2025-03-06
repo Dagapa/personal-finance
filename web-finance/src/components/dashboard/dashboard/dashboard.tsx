7d28bd554bb84d9f2c7290392ce68a32ac73de3c
David Esteban Gallego <dgallego@grupoasd.com>
2025-03-05 20:47:51 -0500

    feat: refactor Dashboard and CreditCard components for improved readability and styling

--- web-finance/src/components/dashboard/dashboard/dashboard.tsx
+++ web-finance/src/components/dashboard/dashboard/dashboard.tsx
@@ -2,34 +2,32 @@ import { CreditCard } from '../../shared/creditCard/creditCard';
 import styles from './styles.module.css';
 
 export const Dashboard = () => {
-  return (
-    <section className={styles.dashboard}>
-      <div className={styles.head_boxes}>
-        <h2>Balance</h2>
-      </div>
-      <div className={styles.head_boxes}>
-        <h2>Resumen</h2>
-      </div>
-      <div className={styles.summary}>
-        <h2 className="text-xl">Transacciones de hoy</h2>
-        <ul className="flex flex-col gap-4 w-full">
-          <li className="flex items-center center gap-1.5 text-sm text-zinc-300">
-            <span>$ -3000</span>
-            <p>House reparations</p>
-          </li>
-          <li className="flex items-center center gap-2 text-sm text-zinc-300">
-            <span>$ -1000</span>
-            <p>Travel to the Europe</p>
-          </li>
-          <li className="flex items-center center gap-2 text-sm text-zinc-300">
-            <span>$ -20</span>
-            <p>Buy cookies</p>
-          </li>
-        </ul>
-      </div>
-      <CreditCard
-        bankName="Davivienda"
-      />
-    </section>
-  );
+	return (
+		<section className={styles.dashboard}>
+			<div className={styles.head_boxes}>
+				<h2>Balance</h2>
+			</div>
+			<div className={styles.head_boxes}>
+				<h2>Resumen</h2>
+			</div>
+			<div className={styles.summary}>
+				<h2 className="text-xl">Transacciones de hoy</h2>
+				<ul className="flex flex-col gap-4 w-full">
+					<li className="flex items-center center gap-1.5 text-sm text-zinc-300">
+						<span>$ -3000</span>
+						<p>House reparations</p>
+					</li>
+					<li className="flex items-center center gap-2 text-sm text-zinc-300">
+						<span>$ -1000</span>
+						<p>Travel to the Europe</p>
+					</li>
+					<li className="flex items-center center gap-2 text-sm text-zinc-300">
+						<span>$ -20</span>
+						<p>Buy cookies</p>
+					</li>
+				</ul>
+			</div>
+			<CreditCard bankName="Davivienda" />
+		</section>
+	);
 };
