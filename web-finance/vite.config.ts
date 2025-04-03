6e9792db15314ba03d2827d601aabd18efd43ee0
David Esteban Gallego <dgallego@grupoasd.com>
2025-04-02 21:26:17 -0500

    feat: agregar alias de módulos en la configuración de TypeScript y Vite para mejorar la resolución de rutas

--- web-finance/vite.config.ts
+++ web-finance/vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@models': '/src/models',
+			'@shared': '/src/components/shared',
+			'@dashboard': '/src/components/dashboard',
+			'@transaction': '/src/components/transaction',
+			'@hooks': '/src/hooks'
+		}
+	}
 });
