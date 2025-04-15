a3973e3c9642e73e95b6cb70fa7a7b3504f09865
David Esteban Gallego <dgallego@grupoasd.com>
2025-04-15 18:32:10 -0500

    remove: delete unncesary css

--- web-finance/src/App.tsx
+++ web-finance/src/App.tsx
@@ -11,19 +11,14 @@ function App() {
 	};
 
 	return (
-		<main className="bg-slate-800 text-white min-w-screen min-h-screen relative flex flex-col items-center justify-center p-15">
+		<main>
 			<Header />
 			<Dashboard />
 
-			<div
-				className={`fixed bg-black bg-opacity-10 flex items-center justify-center transition-opacity duration-300 ${isOpenModal ? 'opacity-100 visible' : 'opacity-0 invisible'}`}
-			>
-				<div
-					className={`bg-white text-black p-6 rounded-lg shadow-lg transition-transform duration-300 ${isOpenModal ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}`}
-				>
+			<div>
+				<div>
 					<button
 						type="button"
-						className="absolute top-3 right-3 text-2xl cursor-pointer"
 						onClick={handleOpenModal}
 					>
 						X
@@ -35,7 +30,6 @@ function App() {
 			{!isOpenModal && (
 				<button
 					type="button"
-					className="absolute right-15 bottom-15 px-3 py-2 bg-blue-600 text-white rounded-4xl shadow-md transition-transform duration-300 hover:scale-105 cursor-pointer"
 					onClick={handleOpenModal}
 				>
 					Agregar transacción
