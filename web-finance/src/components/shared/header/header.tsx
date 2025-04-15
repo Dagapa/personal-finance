632e8a97613de5bf38211d9c0908483050eab2dd
David Esteban Gallego <dgallego@grupoasd.com>
2025-04-15 18:32:25 -0500

    refactor: change styles to tailwind

--- web-finance/src/components/shared/header/header.tsx
+++ web-finance/src/components/shared/header/header.tsx
@@ -1,17 +1,15 @@
-import styles from './styles.module.css';
-
 export const Header = () => {
 	return (
-		<header className={styles.header}>
+		<header className='flex justify-between items-center p-4 w-full h-14 rounded-md shadow-2xs'>
 			<div>
 				<h2>Header</h2>
 			</div>
-			<nav>
-				<ul>
-					<li>Home</li>
-					<li>Transactions</li>
-					<li>Accounts</li>
-					<li>budget</li>
+			<nav className="flex justify-center items-center w-full h-full">
+				<ul className="flex justify-center items-center gap-4">
+					<li className="cursor-pointer text-sm">Home</li>
+					<li className="cursor-pointer text-sm">Transactions</li>
+					<li className="cursor-pointer text-sm">Accounts</li>
+					<li className="cursor-pointer text-sm">budget</li>
 				</ul>
 			</nav>
 			<div>
