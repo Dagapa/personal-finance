a1e9b761a9407d6e7ba60dd53d3a4300424e21d3
David Esteban Gallego <dgallego@grupoasd.com>
2025-04-16 21:35:07 -0500

    refactor: change dashboard head styles

--- web-finance/src/components/dashboard/dashboardHead/dashboardHead.tsx
+++ web-finance/src/components/dashboard/dashboardHead/dashboardHead.tsx
@@ -2,7 +2,7 @@ import { DashboardHeadItem } from '@dashboard/dashboardHeadItem/dashboardHeadIte
 
 export const DashboardHead = () => {
 	return (
-		<section className="col-span-1 grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-2 rounded-xl p-4 w-full h-full">
+		<section className="col-span-1 grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-4 w-full h-full">
 			<DashboardHeadItem title="Balance Mensual" amount="+$25.000" variation="+10% vs el mes anterior" />
 			<DashboardHeadItem title="Saldo Disponible" amount="+$25.000" variation="-5% vs el mes anterior" />
 			<DashboardHeadItem title="Gastos totales" amount="+$25.000" variation="+5% vs el mes anterior" />
