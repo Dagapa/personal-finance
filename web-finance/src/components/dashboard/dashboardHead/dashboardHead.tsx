6317eb0a33d128abb16fc843592f59c955d56b38
David Esteban Gallego <dgallego@grupoasd.com>
2025-04-16 20:53:53 -0500

    feat: modularice head items dashboard

--- web-finance/src/components/dashboard/dashboardHead/dashboardHead.tsx
+++ web-finance/src/components/dashboard/dashboardHead/dashboardHead.tsx
@@ -1,47 +1,12 @@
-import styles from './styles.module.css';
+import { DashboardHeadItem } from '@dashboard/dashboardHeadItem/dashboardHeadItem';
 
 export const DashboardHead = () => {
 	return (
-		<section className="col-span-1 grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-2 border-2 rounded-xl p-4 w-full h-full">
-			<article className="flex flex-col justify-center items-start rounded-md shadow-2xs">
-				<header>
-					<h2>Balance Mensual</h2>
-				</header>
-				<div>
-					<p>+$25.000</p>
-					<span>+10% vs el mes anterior</span>
-				</div>
-			</article>
-
-			<article className={styles.article}>
-				<header>
-					<h2>Saldo Disponible</h2>
-				</header>
-				<div>
-					<p>+$25.000</p>
-					<span>-5% vs el mes anterior</span>
-				</div>
-			</article>
-
-			<article className={styles.article}>
-				<header>
-					<h2>Gastos totales</h2>
-				</header>
-				<div>
-					<p>+$25.000</p>
-					<span>+5% vs el mes anterior</span>
-				</div>
-			</article>
-
-			<article className={styles.article}>
-				<header>
-					<h2>Ahorros</h2>
-				</header>
-				<div>
-					<p>+$25.000</p>
-					<span>+5000 este mes</span>
-				</div>
-			</article>
+		<section className="col-span-1 grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-2 rounded-xl p-4 w-full h-full">
+			<DashboardHeadItem title="Balance Mensual" amount="+$25.000" variation="+10% vs el mes anterior" />
+			<DashboardHeadItem title="Saldo Disponible" amount="+$25.000" variation="-5% vs el mes anterior" />
+			<DashboardHeadItem title="Gastos totales" amount="+$25.000" variation="+5% vs el mes anterior" />
+			<DashboardHeadItem title="Ahorros" amount="+$25.000" variation="+5000 este mes" />
 		</section>
 	);
 };
---
+++ web-finance/src/components/dashboard/dashboardHeadItem/dashboardHeadItem.tsx
@@ -0,0 +1,22 @@
+interface DashboardHeadItemProps {
+  title: string;
+  amount: string;
+  variation: string;
+}
+
+export const DashboardHeadItem = ({ title, amount, variation }: DashboardHeadItemProps) => {
+  const amountColor = amount.startsWith('-') ? 'text-red-400' : 'text-green-600';
+  const variationColor = variation.startsWith('-') ? 'text-red-400' : 'text-green-600';
+
+  return (
+    <article className="flex flex-col justify-around items-center rounded-md shadow-2xs bg-gray-900 transform transition-transform duration-300 hover:scale-102">
+      <header className="flex justify-center items-start w-full h-fit py-4">
+        <h2 className="text-2xl font-bold">{title}</h2>
+      </header>
+      <div>
+        <p className={`text-xl ${amountColor}`}>{amount}</p>
+        <span className={`text-sm ${variationColor}`}>{variation}</span>
+      </div>
+    </article >
+  )
+}
