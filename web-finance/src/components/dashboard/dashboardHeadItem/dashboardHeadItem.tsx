9a1adcf3f785b8f53098b8e731d68b18a517a22f
David Esteban Gallego <dgallego@grupoasd.com>
2025-04-16 21:36:49 -0500

    styles: change to correct rounded in dashboard head item

--- web-finance/src/components/dashboard/dashboardHeadItem/dashboardHeadItem.tsx
+++ web-finance/src/components/dashboard/dashboardHeadItem/dashboardHeadItem.tsx
@@ -9,7 +9,7 @@ export const DashboardHeadItem = ({ title, amount, variation }: DashboardHeadIte
   const variationColor = variation.startsWith('-') ? 'text-red-400' : 'text-green-600';
 
   return (
-    <article className="flex flex-col justify-around items-center rounded-md shadow-2xs bg-gray-900 transform transition-transform duration-300 hover:scale-102">
+    <article className="flex flex-col justify-around items-center rounded-4xl shadow-2xs bg-gray-900 transform transition-transform duration-300 hover:scale-102">
       <header className="flex justify-center items-start w-full h-fit py-4">
         <h2 className="text-2xl font-bold">{title}</h2>
       </header>
